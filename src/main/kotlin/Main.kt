272b7d89ee5f50c44afb11deda4a91f4ace380e0
Jonah <jonahodongo19@gmail.com>
2023-07-27 10:37:12 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,30 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun solve (a: Double, b: Double, operator: Char): Double {
+    return when (operator) {
+        '*' -> a * b
+        '+' -> a + b
+        '-' -> a - b
+        '/' -> {
+            if (b != 0.0) {
+                a / b
+            } else {
+                throw IllegalArgumentException("Cannot divide by zero.")
+            }
+        }
+        else -> throw IllegalArgumentException("Invalid operator: $operator")
+    }
+}
+
+fun main() {
+    val num1 = 10.0
+    val num2 = 5.0
+    val operator = '+'
+    // Change this character to *, +, -.
+
+    if {
+        val result = solve (num1, num2, operator)
+        println("Result: $result")
+    } catch (e: IllegalArgumentException) {
+        println("Error: ${e.message}")
+    }
 }
